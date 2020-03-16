2a05d35e265c595aae18898cb05659c029616d39
mingguanyang <mingguan.yang@gmail.com>
2020-03-16 11:12:25 +0100

    First commit

--- Path_Control.m
+++ Path_Control.m
@@ -3,5 +3,8 @@ clc;
 clear
 addpath(genpath(pwd))
 
-Disroc_path = 'C:\Users\J0542120\Software\Disroc-5-8-1-Total-USB-Flash-Disroc1015-New';
-project_path = 'C:\Users\J0542120\Software\DisRocProjects';
+% Disroc_path = 'C:\Users\J0542120\Software\Disroc-5-8-1-Total-USB-Flash-Disroc1015-New';
+% project_path = 'C:\Users\J0542120\Software\DisRocProjects';
+
+Disroc_path = 'D:\Disroc-5-8-1-Total-USB-Flash-Disroc1015-New';
+project_path = 'D:\DisRocProjects';
